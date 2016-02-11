6916f531a1ca81a48d43e11dcfc01889a28b78ec
Masahiro Nakashima <gutnks19@gmail.com>
2016-02-12 01:56:27 +0900

    add StarFigure.java

---
+++ CS1_sections/StarFigure.java
@@ -0,0 +1,29 @@
+/*
+ * Write a program in a class named StarFigure that produces the following output using for loops.
+ * ////////////////\\\\\\\\\\\\\\\\
+ * ////////////********\\\\\\\\\\\\
+ * ////////****************\\\\\\\\
+ * ////************************\\\\
+ * ********************************
+ */
+
+class StarFigure {
+
+public static void main(String[] args) {
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < (4 - i) * 4; j++){
+            System.out.print('/');
+        }
+        for(int j = 0; j < 8*i; j++){
+            System.out.print('*');
+        }
+        for(int j = 0; j < (4 - i) * 4; j++){
+            System.out.print('\\');
+        }
+
+        System.out.println();
+    }
+
+    }
+
+}
