1f93a9fd4ae8099bbacd27f97a5cac0dc482a5e3
Masahiro Nakashima <gutnks19@gmail.com>
2016-02-12 02:01:33 +0900

    fix StarFigure.java

--- CS1_sections/StarFigure.java
+++ CS1_sections/StarFigure.java
@@ -9,20 +9,21 @@
 
 class StarFigure {
 
-public static void main(String[] args) {
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < (4 - i) * 4; j++){
-            System.out.print('/');
-        }
-        for(int j = 0; j < 8*i; j++){
-            System.out.print('*');
-        }
-        for(int j = 0; j < (4 - i) * 4; j++){
-            System.out.print('\\');
-        }
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < (4 - i) * 4; j++){
+                System.out.print('/');
+            }
+            for(int j = 0; j < 8*i; j++){
+                System.out.print('*');
+            }
+            for(int j = 0; j < (4 - i) * 4; j++){
+                System.out.print('\\');
+            }
 
-        System.out.println();
-    }
+            System.out.println();
+
+        }
 
     }
 
